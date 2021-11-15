3752d0cf648da6a5546819c70127dadb3497a9da
kosiannpann <sunshainekh@gmail.com>
2021-11-15 19:26:51 +0900

    rename app account

--- matching_site/settings.py
+++ matching_site/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'matching_set.apps.MatchingSetConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
